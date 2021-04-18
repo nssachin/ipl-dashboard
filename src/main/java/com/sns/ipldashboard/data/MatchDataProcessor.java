5754c4f788c5a6f660b90d1fa752ca4b7992f1c3
SachinNS <sachinjois@gmail.com>
2021-04-18 19:59:00 +0530

    Batch program to read(csv) and write ipl match data to db on startup.

---
+++ src/main/java/com/sns/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,54 @@
+package com.sns.ipldashboard.data;
+
+
+import com.sns.ipldashboard.model.Match;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+    private static final Logger log = LoggerFactory.getLogger("MatchDataProcessor");
+
+
+    @Override
+    public Match process(MatchInput matchInput) throws Exception {
+
+        Match match = new Match();
+        match.setId(Long.parseLong(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+
+        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+        match.setVenue(matchInput.getVenue());
+
+        String firstInningsTeam;
+        String secondInningsTeam;
+        if ("bat".equals(matchInput.getToss_decision())) {
+            firstInningsTeam = matchInput.getToss_winner();
+            secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) ?
+                    matchInput.getTeam2() : matchInput.getTeam1();
+        } else {
+            secondInningsTeam = matchInput.getToss_winner();
+            firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) ?
+                    matchInput.getTeam2() : matchInput.getTeam1();
+        }
+
+        match.setTeam1(firstInningsTeam);
+        match.setTeam2(secondInningsTeam);
+
+        match.setTossWinner(matchInput.getToss_winner());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setResult(matchInput.getResult());
+        match.setMatchWinner(matchInput.getWinner());
+        match.setResultMargin(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
+
+        return match;
+    }
+}
